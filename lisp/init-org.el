b41e46b15d72e696e09fa6630156c5e236cf1ec5
HopeForToday <novisionnohope@outlook.com>
2019-04-13 17:05:00 +0800

    :sparkles: (Org) Add more babel languages
    and aganda config, `org-ellipsis`.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -6,19 +6,25 @@
   (require 'org-re-reveal)
   :config
   (setq org-plantuml-jar-path
-	(expand-file-name "plantuml.1.2018.13.jar" org-directory))
+        (expand-file-name "plantuml.1.2018.13.jar" org-directory))
+  (setq org-agenda-files `(,(expand-file-name "agenda" org-directory)))
+  (setq org-ellipsis "⤵")
   (setq org-todo-keywords '((sequence "TODO" "DOING" "|" "DONE" "ABORT(a)")))
   (setq org-todo-keyword-faces '(("DOING" . "purple") ("ABORT" . "sea green")))
   (org-babel-do-load-languages
         'org-babel-load-languages
         '((emacs-lisp . t)
+          (ledger . t)
+          (plantuml . t)
           (python . t)
-          (plantuml . t))))
+          (shell . t)
+          )))
+
 
 (use-package org-brain
   :config
   (setq org-id-locations-file
-	(expand-file-name ".org-id-locations" org-brain-path)))
+        (expand-file-name ".org-id-locations" org-brain-path)))
 
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode))
