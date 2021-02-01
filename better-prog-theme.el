4a58287ad3382b735114fc1c41c3e75720c179ed
ionling <novisionnohope@outlook.com>
2021-02-01 21:08:57 +0800

    ✨ Add better-prog theme

---
+++ better-prog-theme.el
@@ -0,0 +1,11 @@
+(deftheme better-prog
+  "Better styles for prog-mode.")
+
+
+(custom-theme-set-faces
+ 'better-prog
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-string-face ((t (:slant italic)))))
+ 
+
+(provide-theme 'better-prog)
