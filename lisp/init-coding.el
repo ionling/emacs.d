f7751767f0110c78c15b57112bde228dd511a263
ionling <soarvision@outlook.com>
2022-03-25 23:15:48 +0800

    ✨ (coding) Add flycheck and restclient module

--- lisp/init-coding.el
+++ lisp/init-coding.el
@@ -37,6 +37,31 @@
     (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)))
 
 
+(v-defmodule flycheck
+  (use-package flycheck
+    :defer 4
+    :custom
+    (flycheck-emacs-lisp-load-path 'inherit)
+    :general
+    (vision-map
+     "fl" #'flycheck-list-errors
+     "fn" #'flycheck-next-error
+     "fp" #'flycheck-previous-error)
+    :config
+    (global-flycheck-mode)
+    (with-eval-after-load 'popwin
+      (push "*Flycheck errors*" popwin:special-display-config)))
+
+
+  (use-package flycheck-posframe
+    :unless use-lsp
+    :hook (flycheck-mode . flycheck-posframe-mode)
+    :custom
+    (flycheck-posframe-position 'window-center)
+    :config
+    (flycheck-posframe-configure-pretty-defaults)))
+
+
 (v-defmodule lsp
   (use-package lsp-ivy)
 
@@ -63,6 +88,15 @@
     ;; the some of the language server responses are in 800k - 3M range.
     (setq read-process-output-max (* 1024 1024)) ; 1MiB
     (general-def "C-c ;" lsp-command-map)))
+
+
+(v-defmodule restclient
+  (use-package restclient
+    :init
+    (with-eval-after-load 'evil
+      (add-to-list 'evil-normal-state-modes 'restclient-mode)))
+
+  (use-package ob-restclient))
 
 
 (provide 'init-coding)
